19161dd63e95404130d94f11197a5e3a297e2d40
Varick Lim <varicklsr@gmail.com>
2024-03-31 15:38:36 +0800

    Add supabase and display puzzle data

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  return <h1 className="text-3xl font-bold">Hello world!</h1>;
+import { createClient } from "@/utils/supabase/client";
+
+export default async function Home() {
+  const supabase = createClient();
+
+  const { data: puzzle } = await supabase
+    .from("sudoku_puzzles")
+    .select()
+    .limit(1)
+    .maybeSingle();
+
+  return (
+    <main>
+      <h1 className="text-3xl font-bold">Hello world!</h1>
+      <code>{JSON.stringify(puzzle)}</code>
+    </main>
+  );
 }
