1340be135ad7f1ce18453ecd028d9e30ba609758
Varick Lim <varicklsr@gmail.com>
2024-04-02 17:58:40 +0800

    Add test cases for Controls

--- app/_components/Controls.test.jsx
+++ app/_components/Controls.test.jsx
@@ -1,5 +1,5 @@
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
+import { render, fireEvent, screen } from "@testing-library/react";
 import Controls from "./Controls";
 
 describe("Controls", () => {
@@ -12,5 +12,79 @@ describe("Controls", () => {
     render(<Controls />);
     const buttons = screen.getAllByRole("button");
     expect(buttons).toHaveLength(4);
+  });
+
+  it("calls the restart function when the restart button is clicked", () => {
+    const mockRestart = jest.fn();
+    render(<Controls restart={mockRestart} />);
+    const restartButton = screen.getByTestId("restart");
+    restartButton.click();
+    expect(mockRestart).toHaveBeenCalled();
+  });
+
+  it("calls the undo function when the undo button is clicked", () => {
+    const mockNewGame = jest.fn();
+    render(<Controls undo={mockNewGame} />);
+    const undoButton = screen.getByTestId("undo");
+    undoButton.click();
+    expect(mockNewGame).toHaveBeenCalled();
+  });
+
+  it("calls the toggleShowConflicts function when the toggle button is clicked", () => {
+    const mockUndo = jest.fn();
+    render(<Controls toggleShowConflicts={mockUndo} />);
+    const toggleButton = screen.getByTestId("toggle");
+    toggleButton.click();
+    expect(mockUndo).toHaveBeenCalled();
+  });
+
+  it("calls the newGame function when the new button is clicked", () => {
+    const mockToggleShowConflicts = jest.fn();
+    render(<Controls newGame={mockToggleShowConflicts} />);
+    const newButton = screen.getByTestId("new");
+    newButton.click();
+    expect(mockToggleShowConflicts).toHaveBeenCalled();
+  });
+
+  it("displays the correct text when showConflicts is true", () => {
+    render(<Controls showConflicts={true} />);
+    const toggleButton = screen.getByTestId("toggle");
+    expect(toggleButton).toHaveTextContent("Hide warnings (W)");
+  });
+
+  it("displays the correct text when showConflicts is false", () => {
+    render(<Controls showConflicts={false} />);
+    const toggleButton = screen.getByTestId("toggle");
+    expect(toggleButton).toHaveTextContent("Show warnings (W)");
+  });
+
+  it("should call the restart function when r key is pressed", () => {
+    const mockRestart = jest.fn();
+    const { container } = render(<Controls restart={mockRestart} />);
+    fireEvent.keyUp(container, { key: "r" });
+    expect(mockRestart).toHaveBeenCalled();
+  });
+
+  it("should call the newGame function when n key is pressed", () => {
+    const mockNewGame = jest.fn();
+    const { container } = render(<Controls newGame={mockNewGame} />);
+    fireEvent.keyUp(container, { key: "n" });
+    expect(mockNewGame).toHaveBeenCalled();
+  });
+
+  it("should call the undo function when z key is pressed", () => {
+    const mockUndo = jest.fn();
+    const { container } = render(<Controls undo={mockUndo} />);
+    fireEvent.keyUp(container, { key: "z" });
+    expect(mockUndo).toHaveBeenCalled();
+  });
+
+  it("should call the toggleShowConflicts function when w key is pressed", () => {
+    const mockToggleShowConflicts = jest.fn();
+    const { container } = render(
+      <Controls toggleShowConflicts={mockToggleShowConflicts} />
+    );
+    fireEvent.keyUp(container, { key: "w" });
+    expect(mockToggleShowConflicts).toHaveBeenCalled();
   });
 });
--- app/_components/Controls.tsx
+++ app/_components/Controls.tsx
@@ -13,10 +13,9 @@ export default function Controls(props: Props) {
 
   useEffect(() => {
     const eventListener = (event: KeyboardEvent) => {
-      console.log("KeyUp:", event.key);
       if (event.key === "r") restart(); // TODO: Add confirmation
       if (event.key === "n") newGame(); // TODO: Add confirmation
-      if (event.key === "h") toggleShowConflicts(); // TODO: Add confirmation
+      if (event.key === "w") toggleShowConflicts(); // TODO: Add confirmation
       if (event.key === "z") undo();
     };
     window.addEventListener("keyup", eventListener);
@@ -27,11 +26,17 @@ export default function Controls(props: Props) {
 
   return (
     <div className="flex flex-row gap-4 mt-2 justify-center items-center">
-      <button onClick={restart}>Restart (R)</button>
-      <button onClick={newGame}>New Game (N)</button>
-      <button onClick={undo}>Undo (Z)</button>
-      <button onClick={toggleShowConflicts}>
-        {showConflicts ? "Hide Hints (H)" : "Show Hints (H)"}
+      <button data-testid="restart" onClick={restart}>
+        Restart (R)
+      </button>
+      <button data-testid="new" onClick={newGame}>
+        New Game (N)
+      </button>
+      <button data-testid="undo" onClick={undo}>
+        Undo (Z)
+      </button>
+      <button data-testid="toggle" onClick={toggleShowConflicts}>
+        {showConflicts ? "Hide warnings (W)" : "Show warnings (W)"}
       </button>
     </div>
   );
